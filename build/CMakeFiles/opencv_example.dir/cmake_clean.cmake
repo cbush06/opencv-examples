0744172445f49518e1875b0fe6c816252bc43366
Clinton Bush <cbush06@gmail.com>
2018-11-20 19:38:43 -0500

    initial commit

---
+++ build/CMakeFiles/opencv_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv_example.dir/main.cpp.o"
+  "bin/opencv_example.pdb"
+  "bin/opencv_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
